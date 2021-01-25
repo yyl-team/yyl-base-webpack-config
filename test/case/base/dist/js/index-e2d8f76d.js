ca10dd10d12825c2b755e6c94f08bdc77c3ba188
liudaojie <liudaojie@yy.com>
2021-01-25 11:14:05 +0800

    feat: 调整配置

--- test/case/base/dist/js/index-e2d8f76d.js
+++
@@ -1,7 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!***********************************!*\
-  !*** ./src/entry/index/index.tsx ***!
-  \***********************************/
-throw new Error("Module parse failed: Unexpected token (10:2)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| \n| const App = (\n>   <Router>\n|     <Switch>\n|       {routes.map((item, index) => {");
-/******/ })()
-;
